7a763691a44d94881e1b83e2b99146e836c22cff
wuhuarou <wobusbz2019@qq.com>
2020-10-03 19:18:09 +0800

    闭包

---
+++ wuhuarou_closure/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let use_closure = || {
+        println!("hello world");
+    };
+    use_closure();
+    println!("Hello, world!");
+
+    // 闭包会为每个函数值和返回值类型推到一个具体的类型，但是不能推导两次
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    let add_one_v3 = |x| x + 1;
+    let add_one_v4 = |x| x + 1;
+    println!("{}",add_one_v1(1));
+    println!("{}",add_one_v2(2));
+    println!("{}",add_one_v3(3));
+    println!("{}",add_one_v4(4));
+}
+
+fn add_one_v1(x: u32) -> u32 {
+    x + 1
+}
