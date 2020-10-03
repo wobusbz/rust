0cb9502d314f13cb14f4dc61f72c41aeb0de4d4c
wuhuarou <wobusbz2019@qq.com>
2020-10-03 19:19:44 +0800

    life

--- wuhaurou_life/src/main.rs
+++ wuhaurou_life/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn longer_life<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() < y.len() {
         x
@@ -10,15 +12,42 @@ struct User<'a> {
     name: &'a str,
 }
 
+impl<'a> User<'a> {
+    fn do_something(&self) -> i32 {
+        3
+    }
+
+    fn do_something2(&self) -> &str {
+        self.name
+    }
+}
+
+fn function<'a, T: Display>(x: &'a str, y: &'a str, ann: T) -> &'a str {
+    println!("{}", ann);
+    if x.len() < y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let x = String::from("abcde");
     let y = String::from("abc");
     let r = longer_life(x.as_str(), y.as_str());
     println!("{}", r);
     let name = String::from("wuhaurou_life");
-    let user = User{
+    let user = User {
         name: name.as_str(),
     };
     println!("{:#?}", user);
+    println!("{}", user.do_something());
+    println!("{}", user.do_something2());
+
+    let s1 = String::from("i am s1");
+    let s2 = String::from("i am s2, hello world");
+    let ann = 129;
+    let r1 = function(s1.as_str(), s2.as_str(), ann);
+    println!("{}", r1);
     println!("Hello, world!");
 }
