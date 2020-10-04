2df45f3256a4be16a250f5acd90085e3cbb5856d
wuhuarou <wobusbz2019@qq.com>
2020-10-04 15:00:34 +0800

    box

---
+++ wuhuarou_box/src/main.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    let b = Box::new(5);
+    println!("{}", b);
+
+    println!("Hello, world!");
+
+    use List::Cons;
+    use List::Nil;
+    // let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", list);
+}
